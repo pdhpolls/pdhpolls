a43005f255848ec1b39438522bf52e1c659c2e36
pdhpolls <johnsonc2014@my.fit.edu>
2016-04-02 15:22:09 -0400

    k

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(".up").on("click", function() {
-	goUp(this.firstChild);
+	if(this.parentNode.getAttribute("data-stat") != "up") {
+		this.parentNode.setAttribute("data-stat", "up");
+		this.setAttribute("data-trans", "false");
+		this.parentNode.childNodes[3].setAttribute("data-trans", "true");
+	} else {
+		this.parentNode.setAttribute("data-stat", "");
+		this.setAttribute("data-trans", "true");
+	}
+});
+$(".down").on("click", function() {
+	if(this.parentNode.getAttribute("data-stat") != "down") {
+		this.parentNode.setAttribute("data-stat", "down");
+		this.setAttribute("data-trans", "false");
+		this.parentNode.childNodes[1].setAttribute("data-trans", "true");
+	} else {
+		this.parentNode.setAttribute("data-stat", "");
+		this.setAttribute("data-trans", "true");
+	}
 });
